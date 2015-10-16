2033929e80cede20eceedcbb12902f32427684f4
rivasjavier2@gmail.com <rivasjavier2@gmail.com>
2015-10-16 17:38:40 -0500

    Made a huge progress

--- csci523/project2/p2.py
+++ csci523/project2/p2.py
@@ -2,7 +2,7 @@ import math
 import sys
 from PRIORITYQueue import PriorityQueue
 from random import randint
-
+from copy import copy
 
 
 # Program Starts
@@ -56,11 +56,11 @@ class State(object):
 		else:
 			return False
 	def zeroIndex(self):
-		return self.matrix.index(0)
+		return copy(self.matrix.index(0))
 	def getF(self):
 		return self.h + self.g
 	def getMatrix(self):
-		return self.matrix
+		return copy(self.matrix)
 
 
 def createStatesManhatan(current_state):
@@ -68,10 +68,7 @@ def createStatesManhatan(current_state):
 	zeroPosition = current_state.zeroIndex()
 	mtrx = current_state.getMatrix()
 	# Up
-	print
-	print mtrx
 	newArray = up(current_state.getMatrix(), zeroPosition)
-	print newArray
 	if newArray is not None:
 		newState = State(newArray, depth = current_state.depth +1, parent = current_state)
 		newState.h = manhathanDistance(newArray)
@@ -82,9 +79,7 @@ def createStatesManhatan(current_state):
 		newState.f = newState.getF()
 		children.append(newState)
 	# Down
-	print current_state.getMatrix()
 	newArray = down(current_state.getMatrix(), zeroPosition)
-	print newArray
 	if newArray is not None:
 		newState = State(newArray, depth = current_state.depth +1, parent = current_state)
 		newState.h = manhathanDistance(newArray)
@@ -95,7 +90,6 @@ def createStatesManhatan(current_state):
 		newState.f = newState.getF()
 		children.append(newState)
 	# Left
-	print current_state.getMatrix()
 	newArray = left(current_state.getMatrix(), zeroPosition)
 	if newArray is not None:
 		newState = State(newArray, depth = current_state.depth +1, parent = current_state)
@@ -107,9 +101,7 @@ def createStatesManhatan(current_state):
 		newState.f = newState.getF()
 		children.append(newState)
 	# Right
-	print current_state.getMatrix()
 	newArray = right(current_state.getMatrix(), zeroPosition)
-	print newArray
 	if newArray is not None:
 		newState = State(newArray, depth = current_state.depth +1, parent = current_state)
 		newState.h = manhathanDistance(newArray)
@@ -230,6 +222,21 @@ def manhathanDistance(matrix):
 			manhathan += abs(yMatrix - yGoal) + abs(xMatrix - xGoal)
 	return manhathan
 
+def createMoves(matrix):
+	for i in range(10):
+		rand = randint(1,4)
+		print str(matrix) + str(rand)
+		if rand == 1:
+			up(matrix, matrix.index(0))
+			print str(matrix) + "up"
+		elif rand == 2:
+			down(matrix, matrix.index(0))
+		elif rand == 3:
+			left(matrix, matrix.index(0))
+		else:
+			right(matrix, matrix.index(0))
+	return matrix
+
 
 
 def AStar():
@@ -241,13 +248,13 @@ def AStar():
 	for i in range(len(matrix)):
 		index = randint(0, len(matrix)-1)
 		randmatrix.append(int(matrix.pop(index)))
-	m= map(int,list("123456708"))
-	f = map(int,list("123456708"))
-	initialState = State(m)
-	initialState.f = manhathanDistance(m)
-	test = State(f)
-	print m.__eq__(f)
-	print initialState
+
+	print randmatrix
+	matrixWithMoves = createMoves(randmatrix)
+	print matrixWithMoves
+	initialState = State(matrixWithMoves)
+	initialState.f = manhathanDistance(matrixWithMoves)
+
 
 	if initialState.isGoal():
 		return initialState
@@ -260,17 +267,33 @@ def AStar():
 	frontier.enqueue(initialState)
 	count  = 1
 	while frontier:
+		"""
 		count += 1
 		if count == 5:
 			break
+		"""
 		state = frontier.dequeue()
 		#print state.to_string() + " " + str(state.f)
 		explored[state.to_string()] = 1
 		if state.isGoal():
-			print state
 			return state
 		children = createStatesManhatan(state)
 		for child in children:
+			if child.to_string() in explored:
+				continue
+			if child not in frontier:
+				frontier.enqueue(child)
+			#elif frontier.__getitem__(child).g > child.g:
+			#	continue
+			else:
+				ch = frontier.__getitem__(child)
+				if ch.f > child.f:
+					#print child.to_string() + " " + str(ch.f) + " " + str(child.f)
+					frontier.__delitem__(child)
+					frontier.enqueue(child)
+					ts = frontier.__getitem__(child)
+			"""
+
 			if (child.to_string() not in explored) and (child not in frontier):
 				frontier.enqueue(child)
 			elif child.to_string() in explored:
@@ -281,6 +304,9 @@ def AStar():
 					#print child.to_string() + " " + str(ch.f) + " " + str(child.f)
 					frontier.__delitem__(child)
 					frontier.enqueue(child)
+					ts = frontier.__getitem__(child)
+					#print ts.to_string() + "---" + str(ts.f)
+			"""
 	"""
 	while frontier:
 		x = frontier.dequeue()
@@ -306,6 +332,11 @@ def printSolution(solution):
 		for i in range(pathSize):
 			state = path[pathSize-i-1]
 			print state
+			if i < pathSize-1:
+				print "-----> " + u"\u2193"
+			else:
+				print u"\u2191"*5
+
 		print "Number of trips = " + str(pathSize-1)
 
 
@@ -314,7 +345,7 @@ def main():
 	print "A Star using manhathanDistance:"
 	if solution is not None:
 		printSolution(solution)
-		print "hey"
+		print "We made it :)"
 	else:
 		print 'No solution.'
 
